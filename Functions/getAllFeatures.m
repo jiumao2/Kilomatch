f1f7134b9f3bdbce90e57b97a8a8fe054ff6cc0c
Yue Huang <806628409@qq.com>
2025-05-09 15:40:19 +0800

    Update getAllFeatures.m

--- Functions/getAllFeatures.m
+++ Functions/getAllFeatures.m
@@ -1,24 +1,18 @@
 function [ISI_features, AutoCorr_features, PETH_features] = getAllFeatures(user_settings, spikeInfo)
 % Get all features
-if isfield(spikeInfo, 'ISI') &&...
-        (any(strcmpi(user_settings.motionEstimation.features, 'ISI')) ||...
-        any(strcmpi(user_settings.clustering.features, 'ISI')))
+if isfield(spikeInfo, 'ISI')
     ISI_features = cat(1, spikeInfo.ISI);
 else
     ISI_features = [];
 end
 
-if isfield(spikeInfo, 'AutoCorr') &&...
-        (any(strcmpi(user_settings.motionEstimation.features, 'AutoCorr')) ||...
-        any(strcmpi(user_settings.clustering.features, 'AutoCorr')))
+if isfield(spikeInfo, 'AutoCorr')
     AutoCorr_features = cat(1, spikeInfo.AutoCorr);
 else
     AutoCorr_features = [];
 end
 
-if isfield(spikeInfo, 'PETH') &&...
-        (any(strcmpi(user_settings.motionEstimation.features, 'PETH')) ||...
-        any(strcmpi(user_settings.clustering.features, 'PETH')))
+if isfield(spikeInfo, 'PETH')
     PETH_features = cat(1, spikeInfo.PETH);
 else
     PETH_features = [];
